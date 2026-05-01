61bd92cfa4672645ad2d969475f241e45bca560a
SEMALIGN TAGESSE <zazenaw46zazu@gmail.com>
2026-05-01 13:56:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// (Click tracking  Dynamic animations)
+console.log("Connect page loaded successfully!");
+
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Button click logic
+    });
+});
